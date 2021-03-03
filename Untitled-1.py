b9b440bb8cee52897fd8fa50ff297fc93894df7a
AlinaPashkovskaya <79252062+AlinaPashkovskaya@users.noreply.github.com>
2021-03-03 20:46:14 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+echo "# AlinaPashkovskaya" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AlinaPashkovskaya/AlinaPashkovskaya.git
+git push -u origin main
