5a8002087085edace0fdd7612124aa5bff09fc70
AlinaPashkovskaya <79252062+AlinaPashkovskaya@users.noreply.github.com>
2021-03-03 20:47:10 +0300

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-print("Hello world!")
-echo "# AlinaPashkovskaya" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/AlinaPashkovskaya/AlinaPashkovskaya.git
-git push -u origin main
